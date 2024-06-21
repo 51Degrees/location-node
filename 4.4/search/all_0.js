e9d84bedc0cd8a1295c2e5a14b7a6fd768fef48e
Automation51D <51DCI@51Degrees.com>
2024-06-21 03:34:21 +0000

    REF: Updated documentation.

--- 4.4/search/all_0.js
+++ 4.4/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['constructor',['constructor',['../class_geo_location_pipeline_builder.html#abdbf81c36a73d88011c7ab8b02929396',1,'GeoLocationPipelineBuilder']]]
+  ['constructor',['constructor',['../class_geo_location_cloud.html#acf36e801f41f630c376c11146e863c54',1,'GeoLocationCloud::constructor()'],['../class_geo_location_pipeline_builder.html#abdbf81c36a73d88011c7ab8b02929396',1,'GeoLocationPipelineBuilder::constructor()']]]
 ];
